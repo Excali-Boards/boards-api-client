4c0e283f9cacf7e95ac198383ccbf4162b0ebabc
Digital39999 <github@crni.xyz>
2025-06-07 02:18:11 +0200

    fix: update version to 1.0.4 in package.json and correct displayName type in GetUsersOutput

--- src/classes/users.ts
+++ src/classes/users.ts
@@ -30,7 +30,8 @@ export type UsersFunctionsInput = {
 export type GetUsersOutput = {
 	id: string;
 	email: string;
-	displayName: string | null;
+	avatarUrl: string | null;
+	displayName: string;
 
 	isDev: boolean;
 	isBoardsAdmin: boolean;
